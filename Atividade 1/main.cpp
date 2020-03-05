286a558b109bf8c06a5c190dfc13f3ce165b8c52
paulosgaf <paulosgaf@hotmail.com>
2020-03-05 19:55:18 -0300

    Atualizado

--- Atividade 1/main.cpp
+++ Atividade 1/main.cpp
@@ -9,74 +9,48 @@
 int main()
 {
 
-/*
 
-	Pessoa pessoa1("paulo","sergio");
 
-	pessoa1.setNome("maria");
-	pessoa1.setSobreNome("de lourdes");
+	Pessoa pessoa1("Mario", "Lopes");
+	Funcionario pessoa2("Lucas", "Mendes", 403616, 2000.00);
+	Professor pessoa3("Rafael", "Lira", 1234, 500.00);
 
-	string nome = pessoa1.getNome();
-	string sobrenome = pessoa1.getSobreNome();
-	string nomecompleto = pessoa1.getNomeCompleto();
+	int matricula;
+	double salario, salario1, salario2;
+	string nome, sobrenome, nomecompleto;
 
-	cout << nome << endl;
-	cout << sobrenome << endl;
-	cout << nomecompleto << endl;
-
-	Funcionario pessoa2("paulo","sergio",403616,1000);
-
-	pessoa2.setNome("maria");
-	pessoa2.setSobreNome("sgaf");
-	pessoa2.setMatricula(12345);
-	pessoa2.setSalario(1000);
 
 	nome = pessoa2.getNome();
 	sobrenome = pessoa2.getSobreNome();
 	nomecompleto = pessoa2.getNomeCompleto();
-	int matricula = pessoa2.getMatricula();
-	double salario = pessoa2.getSalario();
-	double salario1 = pessoa2.getSalarioPrimeiraParcela();
-	double salario2 = pessoa2.getSalarioSegundaParcela();
-
-	cout << nome << endl;
-	cout << sobrenome << endl;
-	cout << nomecompleto << endl;
-	cout << matricula << endl;
-	cout << salario << endl;
-	cout << salario1 << endl;
-	cout << salario2 << endl;
-
-	Professor pessoa3("berneck", "castro", 1234, 2000);
-
-	nome = pessoa3.getNome();
-	sobrenome = pessoa3.getSobreNome();
-	nomecompleto = pessoa3.getNomeCompleto();
-	matricula = pessoa3.getMatricula();
-	salario = pessoa3.getSalario();
-	salario1 = pessoa3.getSalarioPrimeiraParcela();
-	salario2 = pessoa3.getSalarioSegundaParcela();
-
-	cout << nome << endl;
-	cout << sobrenome << endl;
-	cout << nomecompleto << endl;
-	cout << matricula << endl;
-	cout << salario << endl;
-	cout << salario1 << endl;
-	cout << salario2 << endl;
-
-*/
-
-	TestaPessoa testa();
-
-	Pessoa pessoa;
+	matricula = pessoa2.getMatricula();
+	salario = pessoa2.getSalario();
+	salario1 = pessoa2.getSalarioPrimeiraParcela();
+	salario2 = pessoa2.getSalarioSegundaParcela();
 
 
-	pessoa = testa.pessoa1();
+	cout << "----------------PESSOA 1----------------" << endl;
+	cout << "Nome: " << nome << endl;
+	cout << "Sobrenome: " << sobrenome << endl;
+	cout << "Nome Completo: " << nomecompleto << endl;
 
+	cout << "----------------PESSOA 2----------------" << endl;
+	cout << "Nome: " << nome << endl;
+	cout << "Sobrenome: " << sobrenome << endl;
+	cout << "Nome Completo: " << nomecompleto << endl;
+	cout << "Matricula: " << matricula << endl;
+	cout << "Salario: " << salario << endl;
+	cout << "Salario Primeira Parcela: " << salario1 << endl;
+	cout << "Salario Segunda Parcela: " << salario2 << endl;
 
-	string nome = pessoa.getNome();
-	cout << nome << endl;
+	cout << "----------------PESSOA 3----------------" << endl;
+	cout << "Nome: " << nome << endl;
+	cout << "Sobrenome: " << sobrenome << endl;
+	cout << "Nome Completo: " << nomecompleto << endl;
+	cout << "Matricula: " << matricula << endl;
+	cout << "Salario: " << salario << endl;
+	cout << "Salario Primeira Parcela: " << salario1 << endl;
+	cout << "Salario Segunda Parcela: " << salario2 << endl;
 
 	return 0;
 }
